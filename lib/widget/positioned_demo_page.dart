d7fdefa7b0019bb17628b1daf3c94906ce811d0b
mazongyao <mazongyao@123.com>
2022-04-19 19:00:54 +0800

    feat: 学习

--- lib/widget/positioned_demo_page.dart
+++ lib/widget/positioned_demo_page.dart
@@ -6,6 +6,46 @@ class PositionedDemoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PositionedDemoPage"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        margin: const EdgeInsets.all(15),
+        child: Stack(
+          children: [
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blue,
+            ),
+            Positioned(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.greenAccent,
+              ),
+              left: MediaQuery.of(context).size.width / 2,
+            ),
+            Positioned(
+              child: MaterialButton(onPressed: () {}, color: Colors.yellow),
+              left: MediaQuery.of(context).size.width / 5,
+              top: MediaQuery.of(context).size.height / 4 * 3,
+            ),
+            Positioned(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.redAccent,
+              ),
+              left: MediaQuery.of(context).size.width / 2 -
+                  Theme.of(context).buttonTheme.minWidth / 2,
+              top: MediaQuery.of(context).size.height / 2 -
+                  MediaQuery.of(context).padding.top -
+                  kToolbarHeight,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
